20fcbf385cef334e99662bb6931998adc0829781
vinimedeiros13 <vinimedeiros7@outlook.com>
2022-03-14 10:01:21 -0300

    test8

--- src/styles/stitches.config.ts
+++ src/styles/stitches.config.ts
@@ -105,7 +105,7 @@ export const dark = createTheme({
   colors: {
     blue: '#61DCFB',
     green: '#04D361',
-    yellow: '#EBA417',
+    yellow: '#EBA418',
     title: '#E1E1E6',
     text: '#A8A8B3',
     shape: '#1F2729',
